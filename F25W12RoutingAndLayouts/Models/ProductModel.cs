b982e50acfaf37a5bd56f96fad5c749fe91ec1e4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-28 14:46:58 -0500

    nested layouts

---
+++ F25W12RoutingAndLayouts/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace F25W12RoutingAndLayouts.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
